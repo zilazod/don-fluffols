73084beb5fa80a7945dc0881be25b5ac1fd2b404
zilazod <82426403+zilazod@users.noreply.github.com>
2021-05-07 09:57:18 +1200

    world and player
    made start to world and started code for player

--- player.gd
+++ player.gd
@@ -1,18 +1,20 @@
 extends KinematicBody
 
 
+const move_speed = 5
+var y_velo = 0
 
 
 
 
-
-
-
-
-
-
-
-
+func _physics_process(delta):
+	var move_dir = 0
+	if Input.is_action_pressed("move_right"):
+		move_dir += 1
+	if Input.is_action_pressed("move_left"):
+		move_dir -= 1
+	
+	move_and_slide(Vector3(move_dir * move_speed, y_velo, 0), Vector3(0,1,0))
 
 
 
