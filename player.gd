a6890a4eb560992c719b1808fb165a9df06f53e4
zilazod <82426403+zilazod@users.noreply.github.com>
2021-05-10 10:41:16 +1200

    world and player
    coded player movement and tested it in world and edited world

--- player.gd
+++ player.gd
@@ -2,9 +2,10 @@ extends KinematicBody
 
 
 const move_speed = 5
+const jump_force = 9
+const gravity = 9.8
+const max_fall_speed = 30
 var y_velo = 0
-
-
 
 
 func _physics_process(delta):
@@ -16,5 +17,16 @@ func _physics_process(delta):
 	
 	move_and_slide(Vector3(move_dir * move_speed, y_velo, 0), Vector3(0,1,0))
 
-
+	var just_jumped = false
+	var grounded = is_on_floor()
+	y_velo -= gravity * delta
+	if y_velo < -max_fall_speed:
+		y_velo = -max_fall_speed
+		
+		
+	if grounded:
+		y_velo = -0.1
+		if Input.is_action_pressed("jump"):
+			y_velo = jump_force
+			just_jumped = true
 
