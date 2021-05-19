7d56c55d299a8b275549966f57caf3071728f0b9
zilazod <82426403+zilazod@users.noreply.github.com>
2021-05-19 12:24:06 +1200

    aaaaaaaaaaaaaa

--- player.gd
+++ player.gd
@@ -1,16 +1,16 @@
 extends KinematicBody
 
-signal health_updated(health)
-signal kelled()
+#signal health_updated(health)
+#signal kelled()
 
 const move_speed = 7
 const jump_force = 7
 const gravity = 9.8
 const max_fall_speed = 30
 var y_velo = 0
-export (float) var max_health = 100
-onready var health = max_health setget _set_health
-onready var invulnerabillity_timer = $"invulnerabillity timer" 
+#export (float) var max_health = 100
+#onready var health = max_health setget _set_health
+#onready var invulnerabillity_timer = $"invulnerabillity timer" 
 
 
 func _physics_process(delta):
@@ -36,16 +36,16 @@ func _physics_process(delta):
 			just_jumped = true
 
 
-func damage(amount):
+#func damage(amount):
 	if invulnerabillity_timer.is_stopped():
 		invulnerabillity_timer.start()
 	_set_health(health - amount)
 
 
-func kill():
+#func kill():
 	pass
 
-func _set_health(value):
+#func _set_health(value):
 	var prev_health = health
 	health = clamp(value, 0, max_health)
 	if health != prev_health:
@@ -53,6 +53,10 @@ func _set_health(value):
 		if health == 0:
 			kill()
 			emit_signal("kelled")
+
+
+ 
+
 
 
 
